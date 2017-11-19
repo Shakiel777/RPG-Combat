4cdde07d71140f24dbcc54d8deae43e1285c1b54
DavidRood <shakiel@roadrunner.com>
2017-11-18 21:48:00 -0500

    working on the basics -  fixed camera - basic geometry - raycasting primer

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿// Copyright 2017 - David Rood
+// A-Gen-C Entertainment (AGC)
+// Shakiel@roadrunner.com
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position - offset;
+    }
+}
---
+++ Assets/_Scripts/Utility.cs
@@ -0,0 +1,10 @@
+﻿// Copyright 2017 - David Rood
+// A-Gen-C Entertainment (AGC)
+// Shakiel@roadrunner.com
+
+public enum Layer
+{
+    Walkable = 8,
+    Enemy = 9,
+    RaycastEndStop = -1
+}
